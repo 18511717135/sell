ddeaed3742bc6af677a732206068413152037854
18511717135 <38939372+18511717135@users.noreply.github.com>
2018-08-08 11:32:58 +0800

    Update SellApplication.java

--- src/main/java/com/example/sell/SellApplication.java
+++ src/main/java/com/example/sell/SellApplication.java
@@ -8,6 +8,6 @@ public class SellApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SellApplication.class, args);
-		System.out.println("");
+		System.out.println("xi");
 	}
 }
